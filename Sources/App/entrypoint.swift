cf662716cbde986b18a32b53085b89fdac0e8661
shooooooma415 <shoma20040415@icloud.com>
2025-03-19 00:04:34 +0900

    build通ったああああああ

--- Sources/App/entrypoint.swift
+++ Sources/App/entrypoint.swift
@@ -4,25 +4,28 @@ import NIOCore
 import NIOPosix
 
 @main
-enum Entrypoint {
+struct Entrypoint {
     static func main() async throws {
+        let app = try await Application.make(Environment.detect())
+
+        defer { 
+            Task.detached { 
+                do {
+                    try await app.asyncShutdown()
+                } catch {
+                    print("Shutdown error: \(error)")
+                }
+            }
+        }
+        
+        app.middleware.use(CORSMiddleware.default)
+        
         let hostname = Environment.get("DATABASE_HOST") ?? "localhost"
         let username = Environment.get("DATABASE_USERNAME") ?? "vapor"
         let password = Environment.get("DATABASE_PASSWORD") ?? "vapor"
         let database = Environment.get("DATABASE_NAME") ?? "vapor"
-
-    try configure(
-        app, hostname: hostname, username: username, password: password, database: database)
-
-    try configureCORS(app)
-        do {
-            try await configure(app)
-            try await app.execute()
-        } catch {
-            app.logger.report(error: error)
-            try? await app.asyncShutdown()
-            throw error
-        }
-        try await app.asyncShutdown()
+        
+        try configure(app, hostname: hostname, username: username, password: password, database: database)
+        try await app.execute()
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,15 +2,6 @@ import Vapor
 
 
 func routes(_ app: Application) throws {
-    let hostname = Environment.get("DATABASE_HOST") ?? "localhost"
-    let username = Environment.get("DATABASE_USERNAME") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "vapor"
-    let database = Environment.get("DATABASE_NAME") ?? "vapor"
-
-    try configure(
-        app, hostname: hostname, username: username, password: password, database: database)
-
-    try configureCORS(app)
 
     app.get { req async in
         "It works!"
